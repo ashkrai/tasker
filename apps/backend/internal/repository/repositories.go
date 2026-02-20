95ecc9a612df4ac855de070b12102a9e7277cbc1
Ashwani Kumar Rai <ashwani.myjobs@gmail.com>
2026-02-20 19:03:56 +0530

    Initial commit

---
+++ apps/backend/internal/repository/repositories.go
@@ -0,0 +1,17 @@
+package repository
+
+import "github.com/ashkrai/tasker/internal/server"
+
+type Repositories struct {
+	Todo     *TodoRepository
+	Comment  *CommentRepository
+	Category *CategoryRepository
+}
+
+func NewRepositories(s *server.Server) *Repositories {
+	return &Repositories{
+		Todo:     NewTodoRepository(s),
+		Comment:  NewCommentRepository(s),
+		Category: NewCategoryRepository(s),
+	}
+}
